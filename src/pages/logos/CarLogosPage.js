56fb952424d6b5ae1f3e7c441c66e9a20c0fbc70
Alex Canales <alexcanales766@gmail.com>
2022-04-09 19:07:43 -0400

    Changed logo import

--- src/pages/logos/CarLogosPage.js
+++ src/pages/logos/CarLogosPage.js
@@ -5,7 +5,7 @@ import Footer from '../../components/Footer';
 import PageHeader from '../../components/PageHeader';
 
 
-import Card from './components/Card';
+import Card from './components/GridCard';
 
 const CarLogosPage = (props) => {
 
