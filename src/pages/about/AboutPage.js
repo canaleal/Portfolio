d18bce194a4a3412071ba483d16a035fc837dd8b
Alex Canales <alexcanales766@gmail.com>
2022-04-09 19:04:45 -0400

    Changed abou tpage import

--- src/pages/about/AboutPage.js
+++ src/pages/about/AboutPage.js
@@ -7,7 +7,7 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import PageHeader from '../../components/PageHeader';
-import Card from './components/Card';
+import Card from './components/GridCard';
 
 const AboutPage = () => {
 
