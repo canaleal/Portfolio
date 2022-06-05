e9547949a2dfc28b1190eb3241c541b912dc570b
Alex Canales <alexcanales766@gmail.com>
2022-06-05 14:09:01 -0400

    Added new project links

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -5,7 +5,7 @@ function ImageCard({
 }) {
   return (
     <a href={containerlink} target="_blank" rel="noreferrer" aria-label="Github" id={id} className={` col-span-${colSpan} `}>
-      <img height="100" width="auto" src={`${imglink}`} alt={`${title}`} className={`img-card img-card-lg ${isNsfw ? 'blur' : ''} shadow-xl `} />
+      <img height="100" width="auto" src={`${imglink}`} alt={`${title}`} className={`img-card img-card-lg ${isNsfw ? 'blur' : ''} shadow-xl `} loading="lazy" />
     </a>
   );
 }
--- src/pages/Images.jsx
+++ src/pages/Images.jsx
@@ -26,14 +26,14 @@ function Gallery() {
 
             <PageHeader title="Web Getter Images" color="bg-green" zHeight="z-10" />
             <AttentionBar message="This page contains images collected from the Web Getter. Only a hand full of images are shown out of 26000." />
-            <GridLayout id="images" columns=" md:grid-cols-8">
+            <GridLayout id="images" columns=" md:grid-cols-3">
               {data.map((imageElement) => (
                 <ImageCard
                   key={imageElement.id}
                   id={imageElement.id}
-                  containerlink={imageElement.container_link}
-                  imglink={imageElement.image_link}
-                  title={imageElement.name}
+                  containerlink={imageElement.post_link}
+                  imglink={imageElement.post_link}
+                  title={imageElement.post_title}
                   isNsfw
                 />
               ))}
